1a939d7e4d582b80b8149af4f06251d15135bafb
Dayeon Moon <0217dayun@naver.com>
2023-07-03 21:56:27 +0900

    tuist.. 다음에 보자 ...

---
+++ Yomang/YomangApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomangApp.swift
+//  Yomang
+//
+//  Created by 제나 on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct YomangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
