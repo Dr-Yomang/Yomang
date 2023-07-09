5823bd49c407ad8da0a98d9c35c82518028d4b98
Rubik <67681580+Hyeneung-Kwon@users.noreply.github.com>
2023-07-09 11:43:29 +0900

    Revert "[FEAT][#5] 필수 패키지 및 모델 추가"

--- Yomang/YomangApp.swift
+++ Yomang/YomangApp.swift
@@ -6,22 +6,9 @@
 //
 
 import SwiftUI
-import Firebase
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
 
 @main
 struct YomangApp: App {
-    
-    // register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
