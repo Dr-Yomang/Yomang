8537bc1866064edb5c985b34b00a93408557d34f
henry.choi <oreobro@naver.com>
2023-07-11 15:45:34 +0900

    First Commit

---
+++ Yomang/LinkView.swift
@@ -0,0 +1,20 @@
+//
+//  LinkView.swift
+//  Yomang
+//
+//  Created by 최민규 on 2023/07/11.
+//
+
+import SwiftUI
+
+struct LinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        LinkView()
+    }
+}
---
+++ Yomang/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Yomang
+//
+//  Created by 최민규 on 2023/07/11.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
