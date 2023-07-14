02ae153be11744dd0cae0c8fa0573ef84c3a5949
henry.choi <oreobro@naver.com>
2023-07-14 15:15:06 +0900

    [FEAT][#16] 요망 탭 애니메이션 구현

--- Yomang/ContentView.swift
+++ Yomang/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
         if !navigate {
             LinkView(navigate: $navigate)
         } else {
-            YomangAnimation()
+            YomangView()
         }
     }
 }
--- Yomang/TabViewStyle.swift
+++ Yomang/TabViewStyle.swift
@@ -16,26 +16,33 @@ struct TabViewStyle: View {
             let size = $0.size
             
             ZStack {
+                    Color.black
+                        .ignoresSafeArea()
                 
-                TabView(selection: $activeTab) {                    
+                TabView(selection: $activeTab) {
+                    YourYomangView()
+                        .tag(Tab.your)
+                        .offsetX(activeTab == Tab.your) { rect in
+                            let minX = rect.minX
+                            let pageOffset = minX - (size.width * CGFloat(Tab.your.index))
+                            let pageProgress = pageOffset / size.width
+                            scrollProgress = max(min(pageProgress, 0), -CGFloat(Tab.allCases.count - 1))
+                        }
                     
-                    ForEach(Tab.allCases, id: \.rawValue) { tab in
-                        TabImageView(tab)
-                            .tag(tab)
-                            .offsetX(activeTab == tab) { rect in
-                                let minX = rect.minX
-                                let pageOffset = minX - (size.width * CGFloat(tab.index))
-                                let pageProgress = pageOffset / size.width
-                                    scrollProgress = max(min(pageProgress, 0), -CGFloat(Tab.allCases.count - 1))
-                            }
-                    }
-                    
+                    MyYomangView()
+                        .tag(Tab.my)
+                        .offsetX(activeTab == Tab.my) { rect in
+                            let minX = rect.minX
+                            let pageOffset = minX - (size.width * CGFloat(Tab.my.index))
+                            let pageProgress = pageOffset / size.width
+                            scrollProgress = max(min(pageProgress, 0), -CGFloat(Tab.allCases.count - 1))
+                        }
                 }
                 .tabViewStyle(.page(indexDisplayMode: .never))
                 
                 TabIndicatorView()
             }
-        }.ignoresSafeArea(.container, edges: .bottom)
+        }.ignoresSafeArea(.container, edges: .all)
     }
     
     @ViewBuilder
@@ -44,46 +51,78 @@ struct TabViewStyle: View {
             let size = $0.size
             let tabWidth = size.width / 3
             
-            HStack(spacing: 0) {
-                ForEach(Tab.allCases, id: \.rawValue) { tab in
-                    Text(tab.rawValue)
-                        .font(.title3.bold())
-                        .foregroundColor(activeTab == tab ? .white : .gray)
-                        .frame(width: tabWidth)
-                        .contentShape(Rectangle())
-                        .onTapGesture {
-                            withAnimation(.easeInOut(duration: 0.3)) {
-                                activeTab = tab
-                                scrollProgress = -CGFloat(tab.index)
-                                tapState.startAnimation()
-                            }
+            ZStack {
+                Image("Yotto_Face")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 70, height: 70)
+                    .offset(x: -5, y: activeTab == .your ? 100 : 300)
+                    .opacity(activeTab == .your ? 1 : 0)
+                    .scaleEffect(activeTab == .your ? 1 : 0)
+                
+                Image("Yotto_Face")
+                    .resizable()
+                    .scaleEffect(x: -1, y: 1)
+                    .scaledToFit()
+                    .frame(width: 70, height: 70)
+                    .offset(x: 5, y: activeTab == .your ? 300 : 100)
+                    .opacity(activeTab == .your ? 0 : 1)
+                    .scaleEffect(activeTab == .your ? 0 : 1)
+                
+                
+                HStack(spacing: 0) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(activeTab == .your ? .white : .gray)
+                            .frame(width: activeTab == .your ? tabWidth-16 : 16, height: activeTab == .your ? 50 : 16)
+                            .offset(x: activeTab == .your ? 0 : 50)
+                        Text(Tab.your.rawValue)
+                            .font(.title3.bold())
+                            .foregroundColor(activeTab == .your ? .black : .clear)
+                            .frame(width: tabWidth, height: 50)
+                            .contentShape(Rectangle())
+                    }
+                    .onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            activeTab = .your
+                            scrollProgress = -CGFloat(Tab.your.index)
+                            tapState.startAnimation()
                         }
+                    }
+                    .offset(y: 150)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(activeTab == .my ? .white : .gray, lineWidth: 2)
+                            .frame(width: activeTab == .my ? tabWidth-16 : 16, height: activeTab == .my ? 50 : 16)
+                            .background(Color.black)
+                            .offset(x: activeTab == .my ? 0 : -50)
                         
+                        Text(Tab.my.rawValue)
+                            .font(.title3.bold())
+                            .foregroundColor(activeTab == .my ? .white : .clear)
+                            .frame(width: tabWidth, height: 50)
+                            .contentShape(Rectangle())
+                    }
+                    .onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            activeTab = .my
+                            scrollProgress = -CGFloat(Tab.my.index)
+                            tapState.startAnimation()
+                        }
+                    }
+                    .offset(y: 150)
                 }
+                .modifier(
+                    AnimationEndCallback(endValue: tapState.progress, onEnd: {
+                        tapState.reset()
+                    })
+                )
+                .frame(width: CGFloat(Tab.allCases.count) * tabWidth)
+                .padding(.leading, tabWidth)
+                .offset(x: scrollProgress * tabWidth)
             }
-            .modifier(
-                AnimationEndCallback(endValue: tapState.progress, onEnd: {
-                    tapState.reset()
-                })
-            )
-            .frame(width: CGFloat(Tab.allCases.count) * tabWidth)
-            .padding(.leading, tabWidth)
-            .offset(x: scrollProgress * tabWidth)
         }
-    }
-    
-    @ViewBuilder
-    func TabImageView(_ tab: Tab) -> some View {
-        GeometryReader {
-            let size = $0.size
-            
-            Image(tab.rawValue)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: size.width, height: size.height)
-                .clipped()
-        }
-        .ignoresSafeArea(.container, edges: .bottom)
     }
 }
 
