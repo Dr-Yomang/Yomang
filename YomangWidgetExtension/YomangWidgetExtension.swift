7932821a62e87ab45c8a0e80c392982e23a2a239
Dayeon Moon <0217dayun@naver.com>
2023-07-18 21:32:44 +0900

    [CHORE][#17] SwiftLint 적용

--- YomangWidgetExtension/YomangWidgetExtension.swift
+++ YomangWidgetExtension/YomangWidgetExtension.swift
@@ -13,12 +13,12 @@ struct Provider: TimelineProvider {
         SimpleEntry(date: Date())
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
@@ -38,7 +38,7 @@ struct SimpleEntry: TimelineEntry {
     let date: Date
 }
 
-struct YomangWidgetExtensionEntryView : View {
+struct YomangWidgetExtensionEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
