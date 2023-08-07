2912e3b02545c1fafce9453fde0d0a1e0f372f23
Dayeon Moon <0217dayun@naver.com>
2023-08-07 19:33:33 +0900

    [feat][#15] 설정 뷰 리디자인 반영

--- Yomang/Sources/View/LoginView.swift
+++ Yomang/Sources/View/LoginView.swift
@@ -18,11 +18,11 @@ struct LoginView: View {
                 .ignoresSafeArea()
             VStack {
                 Spacer()
-                Image(.yotto1)
+                Image(.yottoHeadOnly)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(height: 272)
-                Text(I18N.yomang)
+                Text(String.yomang)
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .bold()
@@ -37,7 +37,7 @@ struct LoginView: View {
                 Text(matchingID ?? "nil")
                     .foregroundColor(.white)
                 
-                Text(I18N.authenticationMessage)
+                Text(String.authenticationMessage)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .font(.caption2)
--- Yomang/Sources/View/SettingView.swift
+++ Yomang/Sources/View/SettingView.swift
@@ -10,72 +10,62 @@ import SwiftUI
 struct SettingView: View {
     private var username = "ZENA"
     var body: some View {
-        NavigationView {
-            List {
-                Section {
+        List {
+            Section {
+                HStack {
+                    Circle()
+                        .foregroundColor(Color.neu500)
+                        .overlay {
+                            Image(String.yottoWithUpperBody)
+                                .resizable()
+                                .scaledToFill()
+                                .clipShape(Circle())
+                        }
+                        .frame(width: 112, height: 112)
+                        .padding(.vertical, 10)
+                    Spacer()
+                }
+            }
+            
+            Section(header: Text(String.headerTitleSettingProfile)) {
+                NavigationLink {
+                    EmptyView()
+                } label: {
                     HStack {
-//                        Image(systemName: "person.fill.xmark")
-                        Text("\(username)")
+                        Image(systemName: .personFill)
+                        Text(String.buttonMyProfile)
                     }
                 }
                 
-                Section {
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        HStack {
-                            Image(systemName: "person.text.rectangle.fill")
-                            Text("닉네임 변경")
-                        }
-                    }
-
-                }
-                
-                Section {
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        HStack {
-                            Image(systemName: "person.fill.xmark")
-                            Text("연결 해제")
-                        }
-                    }
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        HStack {
-                            Image(systemName: "person.line.dotted.person.fill")
-                            Text("연결 해제 복구")
-                        }
-                    }
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        HStack {
-                            Image(systemName: "person.2.wave.2.fill")
-                            Text("새로 연결하기")
-                        }
+            }
+            
+            Section(header: Text(String.headerTitleMyUsage)) {
+                NavigationLink {
+                    EmptyView()
+                } label: {
+                    HStack {
+                        Image(systemName: .person2Fill)
+                        Text(String.buttonConnectPartner)
                     }
                 }
-                    
-                Section {
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        Text("로그아웃")
+                NavigationLink {
+                    EmptyView()
+                } label: {
+                    HStack {
+                        Image(systemName: .bellFill)
+                        Text(String.buttonSettingNotification)
                     }
-                    
-                    NavigationLink {
-                        EmptyView()
-                    } label: {
-                        Text("탈퇴")
-                    }
-
                 }
             }
         }
-        .navigationTitle("설정 및 개인정보")
+        .navigationTitle(String.navigationTitleSetting)
         .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image(systemName: .chevronBackward)
+            }
+        }
     }
 }
 
