4942b03f34f32900e254e7388e8fe0366f54a9ab
khnueng <khnueng@naver.com>
2023-09-01 16:19:35 +0900

    [FEAT][#73] 파트너 연결 설정화면 추가

--- Yomang/Sources/View/SettingView.swift
+++ Yomang/Sources/View/SettingView.swift
@@ -212,8 +212,65 @@ struct SettingView: View {
     }
 }
 
+struct PartnerConnectionSettingView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "chevron.backward")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 32, height: 32)
+                Spacer()
+                Text("파트너 연결")
+                    .bold()
+                    .font(.title2)
+                    .padding(.leading, -32.0)
+                Spacer()
+            }
+            .padding(.bottom, 35)
+            HStack {
+                Text("연결된 파트너")
+                    .padding(.leading, 20)
+                    .foregroundStyle(Color(hex: 0xC7C7CC))
+                Text("까망토끼")
+                    .bold()
+                    .foregroundColor(.black)
+                    .background(                  RoundedRectangle(cornerRadius: 12)
+                        .frame(width: 82, height: 28))
+                    .padding(.leading, 14)
+                    Spacer()
+            }
+            .padding(.bottom,44)
+            Button(action: {
+                
+            }, label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(Color(hex: 0x1C1C1D))
+                    .frame(height: 44)
+                    .padding(.horizontal, 20)
+                    HStack {
+                        Text("연결 끊기")
+                            .font(.subheadline)
+                            .foregroundStyle(Color(hex: 0x8A42FF))
+                            .padding(.leading, 32)
+                        Spacer()
+                        Image(systemName: "chevron.forward")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 18, height: 18)
+                            .foregroundStyle(Color.white)
+                            .padding(.trailing, 25)
+                    }
+                }
+            })
+            Spacer()
+        }
+    }
+}
+
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingView()
+        PartnerConnectionSettingView()
     }
 }
